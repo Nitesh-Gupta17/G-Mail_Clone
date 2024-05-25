bf7ba573c496209cc77a8d773c66a262a1a071b3
Nitesh-Gupta17 <131606845+Nitesh-Gupta17@users.noreply.github.com>
2024-05-25 20:57:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed =new Typed(".multiple-text", {strings: ["Frontend Developer","Coder","Team player","Quick Learner"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
